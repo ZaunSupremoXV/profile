6d5e5c1dc949781cb3732da6dad562644ac2f47b
ZaunSupremoXV <ximenes16sony@gmail.com>
2022-04-02 19:37:09 -0300

    Erick deploy 020420221937

--- lib/app/pages/login/sign_in_controller.dart
+++ lib/app/pages/login/sign_in_controller.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:profile/app/pages/home/home_page.dart';
 import 'package:profile/app/repository/hive_user_repository.dart';
--- lib/app/pages/sign_up/sign_up_controller.dart
+++ lib/app/pages/sign_up/sign_up_controller.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:profile/app/pages/login/sign_in_page.dart';
 import 'package:profile/app/repository/hive_user_repository.dart';
